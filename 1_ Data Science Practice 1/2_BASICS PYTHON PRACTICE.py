e0ea7026e30cf10ac56fde93a2bc161547ed5cc1
Fareeha <fareeha.crypto@gmail.com>
2024-12-27 22:34:48 +0500

    update

--- 1_ Data Science Practice 1/2_BASICS PYTHON PRACTICE.py
+++ 1_ Data Science Practice 1/2_BASICS PYTHON PRACTICE.py
@@ -2,7 +2,7 @@
 
 print ("PATIENT MANAGEMENT SYSTEM")
 
-data= {}
+data= []
 
 def data_entry():
     mr = int(input("Enter MR No.: "))
@@ -13,4 +13,38 @@ def data_entry():
     print (f"The data of patient {name} has been saved.")
 
 def data_search():
-    
+    mr = int(input("Enter MR No.: "))
+    for patient in data:
+        if patient["mr"] == mr:
+            print (f"The details are as follow: \n" 
+                   f"MR No. : {patient["mr"]} \n" 
+                   f"Name: {patient["name"]} \n"
+                   f"Age: {patient["age"]} yrs \n" 
+                   f"Weight: {patient["weight"]}kgs \n")
+            return
+        else:
+            print ("Record not found!")
+
+def full_data():
+    for patient in data:
+        print (data)
+
+while True:
+    print ("1. Add New")
+    print ("2. Search Database")
+    print ("3. View Database")
+    print ("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        data_entry()
+    elif choice == "2":
+        data_search()
+    elif choice == "3":
+        full_data()
+    elif choice == "4":
+        break
+    else:
+        print ("Invalid Choice!")
+
